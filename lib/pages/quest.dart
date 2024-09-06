566bb5a99a06e8640a2e02d7c517a4138539a007
danielwidhiarto <danielwidhiarto@gmail.com>
2024-09-06 16:24:27 +0700

    add 5 pages for navbar

---
+++ lib/pages/quest.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class QuestPage extends StatelessWidget {
+  const QuestPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Quest Page'),
+    );
+  }
+}
